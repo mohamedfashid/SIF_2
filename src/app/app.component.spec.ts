f3439570f5f9a8e8203d8018f1d829babcbf3593
mohamedfashid <mohamedfashid21@gmail.com>
2024-09-23 03:55:59 +0530

    'update'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TheCrispyChicken'`, () => {
+  it(`should have as title 'SIF'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TheCrispyChicken');
+    expect(app.title).toEqual('SIF');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TheCrispyChicken app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('SIF app is running!');
   });
 });
